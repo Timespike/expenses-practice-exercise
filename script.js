e2a016915278dac7827ec8f56a080f64330cfeff
Timespike <50344633+Timespike@users.noreply.github.com>
2022-02-24 22:46:43 -0600

    not sure what I'm doing wrong with the form

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ const form = document.getElementById("expense-form");
 const submitBtn = document.getElementById("submit-button");
 
 function submitForm () {
-    
+    form.preventDefault();
+    console.log(form);
 }
 
 
-submitBtn.addEventListener('click', submitForm())
+submitBtn.addEventListener('click', submitForm(form));
